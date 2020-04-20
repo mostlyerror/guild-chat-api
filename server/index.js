5ecd9c42852c658d8762efede5aab04168ef2a71
Benjamin Poon <benjamintpoon@gmail.com>
2020-04-20 15:45:11 -0600

    add middleware for processing urlencoded json bodies

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const server = express();
 const routes = require("../routes");
 
+server.use(express.urlencoded({ extended: true }))
 server.use(express.json());
 server.use("/api", routes);
 
